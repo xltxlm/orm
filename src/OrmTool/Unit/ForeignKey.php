dd81021e9b2047e84bdc608851f83f237259c955
夏琳泰 <johndoe@example.com>
2016-11-26 19:00:14 +0800

    改进

--- src/OrmTool/Unit/ForeignKey.php
+++ src/OrmTool/Unit/ForeignKey.php
@@ -8,12 +8,17 @@
 
 namespace OrmTool\Unit;
 
+
 class ForeignKey
 {
-    public function abc()
-    {
-        if (1) {
-        } elseif (2) {
+    public function abc(){
+
+        if(1)
+        {
+
+        }elseif (2)
+        {
+
         }
     }
 }
