eb335ee63cbef1e1abfc25315864b96f229aee18
夏琳泰 <johndoe@example.com>
2016-11-26 18:58:19 +0800

    改进

---
+++ src/OrmTool/Unit/ForeignKey.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-11-26
+ * Time: 下午 6:57
+ */
+
+namespace OrmTool\Unit;
+
+
+class ForeignKey
+{
+    public function abc(){
+
+        if(1)
+        {
+
+        }elseif (2)
+        {
+
+        }
+    }
+}
