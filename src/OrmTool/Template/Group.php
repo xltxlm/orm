f5f35eb6ab6ae8f2f9c1f1813a51c57c7ccef4db
夏琳泰 <johndoe@example.com>
2016-11-27 19:52:41 +0800

    改进

---
+++ src/OrmTool/Template/Group.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-11-27
+ * Time: 下午 7:48
+ */
+
+namespace OrmTool\Template;
+
+/**
+ * 支持分组查询，返回二维数组
+ * Class Group
+ * @package OrmTool\Template
+ */
+class Group extends PdoAction
+{
+}
---
+++ src/OrmTool/Template/Model/Group.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-11-27
+ * Time: 下午 7:48
+ */
