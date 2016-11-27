bfa585b705c184de1be41a4ab1a52bd954f132c6
夏琳泰 <johndoe@example.com>
2016-11-27 22:29:52 +0900

    测试travis-ci 构建系统

--- demo/setup/Doc.php
+++ demo/setup/Doc.php
@@ -18,6 +18,6 @@ class Doc extends PdoConfig
     protected $driver = self::MYSQL;
     protected $TNS = '127.0.0.1';
     protected $db = 'doc';
-    protected $username = 'doc';
-    protected $password = '123456';
+    protected $username = 'root';
+    protected $password = '';
 }
