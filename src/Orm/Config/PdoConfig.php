97978c9c88ba72ec86e24d1d33cdbaa9dd5a1f98
夏琳泰 <johndoe@example.com>
2016-11-27 23:41:37 +0900

    测试travis-ci 构建系统 - 导入数据库 13

--- src/Orm/Config/PdoConfig.php
+++ src/Orm/Config/PdoConfig.php
@@ -118,7 +118,7 @@ abstract class PdoConfig
      */
     public function getDb(): string
     {
-        return strtolower(basename(static::class));
+        return strtolower(basename('\\'.static::class));
     }
 
     /**
