16fa1fdd5c833f8e37c20aba7e194f8fa161e123
夏琳泰 <xltxlm@qq.com>
2017-01-12 13:16:58 +0800

    模型:可以取得变量名,可以返回数组格式

--- src/ormTool/Template/Model/Model.tpl.php
+++ src/ormTool/Template/Model/Model.tpl.php
@@ -3,7 +3,10 @@
 <?php /** @var \xltxlm\ormTool\Unit\FieldSchema[] $fieldSchema */?>
 <<?='?'?>php
 
-namespace <?=$this->getDbNameSpace()?>;
+        namespace <?=$this->getDbNameSpace()?>;
+
+        use xltxlm\helper\Hclass\ObjectToArray;
+        use xltxlm\helper\Hclass\CopyObjectAttributeName;
 
 /**
 * Class select
@@ -11,33 +14,46 @@ namespace <?=$this->getDbNameSpace()?>;
 */
 final class <?=ucfirst($tableSchema->getTABLENAME())?>Model
 {
+use ObjectToArray;
+use CopyObjectAttributeName;
+
 <?php foreach ($fieldSchema as $field) {
     ?>
     /** @var string <?=$field->getCOLUMNCOMMENT()?> <?=$field->getCOLUMNTYPE()?> */
     protected $<?=$field->getCOLUMNNAME()?>;
     /**
-     * out:<?=$field->getCOLUMNCOMMENT()?> <?=$field->getCOLUMNTYPE()?>
+    * out:<?=$field->getCOLUMNCOMMENT()?> <?=$field->getCOLUMNTYPE()?>
 
-     * @return string
-     */
+    * @return string
+    */
     public function get<?=ucfirst($field->getCOLUMNNAME())?>()
     {
-        return $this-><?=$field->getCOLUMNNAME()?>;
+    return $this-><?=$field->getCOLUMNNAME()?>;
     }
 
     /**
-     * out:<?=$field->getCOLUMNCOMMENT()?>
+    * out:<?=$field->getCOLUMNCOMMENT()?> <?=$field->getCOLUMNTYPE()?>
 
-     * @param mixed $<?=$field->getCOLUMNNAME()?>
+    * @return string
+    */
+    public function varname<?=ucfirst($field->getCOLUMNNAME())?>()
+    {
+    return (self::selfInstance()->varName(self::selfInstance()-><?=$field->getCOLUMNNAME()?>));
+    }
 
-     * @return $this
-     */
+    /**
+    * out:<?=$field->getCOLUMNCOMMENT()?>
+
+    * @param mixed $<?=$field->getCOLUMNNAME()?>
+
+    * @return $this
+    */
     public function set<?=ucfirst($field->getCOLUMNNAME())?>($<?=$field->getCOLUMNNAME()?>)
     {
-        $this-><?=$field->getCOLUMNNAME()?>=$<?=$field->getCOLUMNNAME()?>;
-        return $this;
+    $this-><?=$field->getCOLUMNNAME()?>=$<?=$field->getCOLUMNNAME()?>;
+    return $this;
     }
-<?php
+    <?php
 
 }?>
 }
